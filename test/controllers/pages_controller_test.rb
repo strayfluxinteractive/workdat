afa265e536add1eab78fa9c1cfb5c4c3dd540c72
Cameron Morris <zephyrz2712@gmail.com>
2014-10-24 19:39:35 -0400

    Single quotes

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,7 +4,7 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "WorkDat"
+    assert_select 'title', 'WorkDat'
   end
 
 end
