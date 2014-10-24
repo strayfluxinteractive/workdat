cf17fe2579ca761ce37273987c8f106f33da841c
Cameron Morris <zephyrz2712@gmail.com>
2014-10-24 19:34:23 -0400

    Some quality stuff

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
+# Tests for the applicationhelper
 class ApplicationHelperTest < ActionController::TestCase
   test "full title helper" do
-    assert_equal full_title, "WorkDat"
-    assert_equal full_title("About"), "About | WorkDat"
+    assert_equal full_title, 'WorkDat'
+    assert_equal full_title('About'), 'About | WorkDat'
   end
-
 end
