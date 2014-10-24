245188aa52264551afd409fd2c30da45ad598051
Cameron Morris <zephyrz2712@gmail.com>
2014-10-24 19:36:40 -0400

    Single quotes

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 # Tests for the applicationhelper
 class ApplicationHelperTest < ActionController::TestCase
-  test "full title helper" do
+  test 'full title helper' do
     assert_equal full_title, 'WorkDat'
     assert_equal full_title('About'), 'About | WorkDat'
   end
