b59eef8716b0d9fad10b3cd4adce135386a8f811
zephyrz4 <zephyrz2712@gmail.com>
2014-10-25 18:44:50 -0400

    Improved google test

--- test/integration/google_login_test.rb
+++ test/integration/google_login_test.rb
@@ -2,22 +2,27 @@ require 'test_helper'
 
 # Test the google login process
 class GoogleLoginTest < ActionDispatch::IntegrationTest
-  # Omniauth
-  OmniAuth.config.test_mode = true
-  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
-      provider: 'google_oauth2',
-      uid: '12345',
-      info: {
-        name: 'John Doe'
-      },
-      credentials: {
-        token: 'token',
-        expires_at: 1_354_920_555
-      }
-  )
+  def setup
+    @user = users(:one)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+        provider: @user.provider, uid: @user.uid,
+        info: { name: @user.name },
+        credentials: {
+          token: @user.oauth_token,
+          expires_at: @user.oauth_expires_at })
+  end
+
   test 'google_signin' do
     get_via_redirect '/auth/google_oauth2', nil, 'omniauth.auth' => OmniAuth.config.mock_auth[:google_oauth2]
     assert_response :success
-    assert_select '#username', 'John Doe'
+    assert_select '#username', @user.name
+    assert_equal session[:user_id], @user.id
+  end
+
+  test 'google_signout' do
+    get_via_redirect signout_path
+    assert_response :success
+    assert_equal session[:user_id], nil
   end
 end
