de7fd7e8fba50c103b8ce501f0864a02140da1b2
zephyrz4 <zephyrz2712@gmail.com>
2014-10-26 11:43:05 -0400

    Changed to logger

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
 # config/unicorn.rb
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
 timeout 15
 preload_app true
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    logger.info 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
@@ -15,7 +15,7 @@ end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    logger.info 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
