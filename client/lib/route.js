8872a221c50435d9bfae085f87616fee1e381701
zephyrz4 <zephyrz2712@gmail.com>
2015-03-10 18:23:50 +0000

    Enforce login and added mongo collections

--- client/lib/route.js
+++ client/lib/route.js
@@ -2,3 +2,17 @@ Router.map(function(){
   this.route("dashboard");
   this.route("landing", {path: '/'});
 });
+
+var requireLogin = function() {
+  if (! Meteor.user()) {
+   // If user is not logged in render landingpage
+   this.render('landing');
+ } else {
+   //if user is logged in render whatever route was requested
+   this.next();
+ }
+}
+// Before any routing run the requireLogin function.
+// Except in the case of "landingpage".
+// Note that you can add more pages in the exceptions if you want. (e.g. About, Faq, contact...)
+Router.onBeforeAction(requireLogin, {except: ['landing']});
